8de72cea8547c8f65c376e258c525131fc672606
gabrielaTena <89794591+gabrielaTena@users.noreply.github.com>
2022-02-08 22:41:11 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,9 @@
+// Make green salsa.
+/ / Wash ingredients
+  / / Chop ingredients
+    / / Cook/fry chiles for 8 minutes
+    / / Add onion and garlic at 6 minutes (once the chiles look golden)
+        / / Once golden, remove from stove
+        / / Brend everything, add salt
+      / / Add a little bit of water
+    / / Try the salsa and add salt if needed
