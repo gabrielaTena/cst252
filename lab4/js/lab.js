f55ac7f8e0b59ecd000eb79e2ffb6bbbb4607f5b
gabrielaTena <89794591+gabrielaTena@users.noreply.github.com>
2022-02-11 23:21:07 -0800

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,9 +1,10 @@
 // Make green salsa.
-/ / Wash ingredients
-  / / Chop ingredients
-    / / Cook/fry chiles for 8 minutes
-    / / Add onion and garlic at 6 minutes (once the chiles look golden)
-        / / Once golden, remove from stove
-        / / Brend everything, add salt
-      / / Add a little bit of water
-    / / Try the salsa and add salt if needed
+// Wash ingredients
+    // Chop ingredients
+      // Cook/fry chiles for 8 minutes
+      // Add onion and garlic at 6 minutes (once the chiles look golden)
+        // Once golden, remove from stove
+        // Brend everything, add salt
+      // Add a little bit of water
+    // Try the salsa and add salt if needed
+//
---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Gabriela Tena <gtenajacobo@csumb.edu>
+ * Created:   02.11.2022
+ * (c) Copyright by Blub Corp.
+ **/
+
+ // Define Variables
+ make = "Ford";
+ model = "Taurus";
+ color = "Rusty";
+ year = 1995;
+
+ // calculate
+ age = 2022 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "years<br>");
