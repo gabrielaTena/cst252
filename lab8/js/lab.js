05a63e5c6044945bed44cb896a78bb97656a4811
gabrielaTena <89794591+gabrielaTena@users.noreply.github.com>
2022-02-28 00:23:51 -0800

    lab8

---
+++ lab8/js/lab.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Gabriela Tena
+ * Created: 27 February 2022
+ * License: Public Domain
+ */
+
+ function labEight(x){
+   return(x * 2);
+ }
+
+ //test function
+ array = [50, 22, 100, 150, 40, 12, 3]
+ console.log("My array", array);
+
+ var result = array.map(labEight);
+ //
+ console.log("Test of correcness of array:", result);
+
+ var result = array.map(function(x){
+   return x + 5;
+ })
+ //should return [55, 27, 105, 155, 45, 17, 8]
+ console.log("Results of array plus 5:", results);
